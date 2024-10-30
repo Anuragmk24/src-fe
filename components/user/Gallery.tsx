36a281b5bf7f872f8708df7a5b208d2372a64b9d
mythri <mythri@kartblue.com>
2024-10-30 17:49:38 +0530

    nature image added

--- components/user/Gallery.tsx
+++ components/user/Gallery.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Gallery() {
   return (
     <div className='w-full'>
-      <img className='w-full' src="/assets/gallery/img1.svg" alt="" />
+      <img className='w-full' src="/assets/gallery/nature.jpg" alt="" />
     </div>
   )
 }
