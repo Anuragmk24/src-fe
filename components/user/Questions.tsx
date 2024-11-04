3f15dce05606cec9f20577320f36beb36158c10b
mythri <mythri@kartblue.com>
2024-11-04 09:25:18 +0530

    note changed

--- components/user/Questions.tsx
+++ components/user/Questions.tsx
@@ -19,7 +19,7 @@ function Questions({ register, question, name }: { register: any; question: stri
             </div>
             {question === 'Do you want accomodation' && (
                 <Points
-                    points="Please note that roomRoom sharing arrangements will be based on the type of booking and group size. For group bookings, rooms are more likely to be allocated according to the number of participants: groups of two will receive double occupancy rooms (with couples receiving first preference), groups of three will be placed in triple occupancy rooms, and groups of four in quadruple occupancy rooms.
+                    points="Room sharing arrangements will be based on the type of booking and group size. For group bookings, rooms are more likely to be allocated according to the number of participants: groups of two will receive double occupancy rooms (with couples receiving first preference), groups of three will be placed in triple occupancy rooms, and groups of four in quadruple occupancy rooms.
 
 Please consider your group size carefully, as changes to room assignments may not be possible once allocated.
 
